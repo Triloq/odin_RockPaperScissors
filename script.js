0e3eb64077edfad9e9e81447067b988b3794a9fd
Triloq <25014506+Triloq@users.noreply.github.com>
2023-04-18 17:24:06 -0500

    Using javascript to set up game with buttons

--- script.js
+++ script.js
@@ -17,50 +17,125 @@ function getComputerChoice() {
    }
 }
 
-function playRound(){
+function playRound(pc_Choice){
     let npc_Choice = getComputerChoice().toLowerCase();
-    let pc_Choice = prompt("Rock... Paper... Scissors... SHOOT!: ").toLowerCase();
+    //let pc_Choice = prompt("Rock... Paper... Scissors... SHOOT!: ").toLowerCase();
     let winner; // 0 - draw, 1 - win, 2 - lose
+    let scoreKeep;
 
-    console.log(npc_Choice);
-    console.log(pc_Choice);
     if (pc_Choice == npc_Choice){
-        winner = "Draw!";
+        winner = "This round is a Draw!";
+        scoreKeep = 'draw';
     }
     else if (pc_Choice == "scissors" && npc_Choice == "rock" ||
             pc_Choice == "paper" && npc_Choice == "scissors" ||
             pc_Choice == "rock" && npc_Choice == "paper")
     {
-        winner = "You Lose! " + npc_Choice.charAt(0).toUpperCase() + npc_Choice.slice(1) +
+        winner = "You Lose this round! " + npc_Choice.charAt(0).toUpperCase() + npc_Choice.slice(1) +
                 " beats " + pc_Choice.charAt(0).toUpperCase() + pc_Choice.slice(1);
-    }
-    // else if (pc_Choice == "paper" && npc_Choice == "scissors"){
-    //     winner = "You Lose! Scissors beats Paper";
-    // }
-    // else if (pc_Choice == "rock" && npc_Choice == "paper"){
-    //     winner = "You Lose! Paper beats Rock";
-    // }
+        scoreKeep = 'lose';
+        }
+    
     else if (npc_Choice == "scissors" && pc_Choice == "rock" ||
             npc_Choice == "paper" && pc_Choice == "scissors" ||
             npc_Choice == "rock" && pc_Choice == "paper")
     {
-        winner = "You Win! " + pc_Choice.charAt(0).toUpperCase() + pc_Choice.slice(1) +
+        winner = "You Win this round! " + pc_Choice.charAt(0).toUpperCase() + pc_Choice.slice(1) +
             " beats " + npc_Choice.charAt(0).toUpperCase() + npc_Choice.slice(1);
+        scoreKeep = 'win';
     }
-    // else if (npc_Choice == "paper" && pc_Choice == "scissors"){
-    //     winner = "You Win! Scissors beats Paper";
-    // }
-    // else if (npc_Choice == "rock" && pc_Choice == "paper"){
-    //     winner = "You Win! Paper beats Rock";
-    // }
-    return winner;
+    
+    return [winner, scoreKeep];
 }
 
-function game() {
-    let keepGoing = "True";
-    while(keepGoing){
-        console.log(playRound());
+// function game() {
+//     let keepGoing = "True";
+//     while(keepGoing){
+//         console.log(playRound());
+//     }}
+// game();
+
+// select the container div
+const div = document.querySelector('div');
+// add a header with start text
+const startText = document.createElement('h3');
+startText.textContent = "Rock, Paper, Scissors... SHOOT!";
+// create 3 buttons
+const btn1 = document.createElement('button');
+btn1.textContent = "Rock";
+btn1.classList.add('rock');
+const btn2 = document.createElement('button');
+btn2.textContent = "Paper";
+btn2.classList.add('paper');
+const btn3 = document.createElement('button');
+btn3.textContent = "Scissors";
+btn3.classList.add('scissors');
+// create div for results
+const results = document.createElement('div');
+results.classList.add('results');
+// add the elements to the container div
+div.appendChild(startText);
+div.appendChild(btn1);
+div.appendChild(btn2);
+div.appendChild(btn3);
+div.appendChild(results);
+// selector for all buttons
+const buttons = document.querySelectorAll('button');
+// selector for results div, create 2 <p> for result and score
+const resultDiv = document.querySelector('.results');
+const resP = document.createElement('p');
+const score = document.createElement('p');
+resP.textContent = "The winner is..."
+score.textContent = "Score: Player -   Computer - ";
+resP.classList.add('results')
+score.classList.add('score');
+resultDiv.appendChild(resP);
+resultDiv.appendChild(score);
+// add final result <p>
+const final = document.createElement('p');
+final.classList.add('final');
+resultDiv.appendChild(final);
+// listener events
+let pcScore = 0;
+let npcScore = 0;
+buttons.forEach(btn => {
+    btn.addEventListener('click', event => {
+        let results = playRound(event.target.classList.value);
+        // console.log(results);
+        resP.textContent = `${results[0]}`;
+        if(results[1] === 'win'){
+            pcScore++;
+        }
+        else if(results[1] === 'lose'){
+            npcScore++;
+        }
+        score.textContent = "Score: "+"Player - "+pcScore+
+            " Computer - "+npcScore;
+        let end = endScore();
+        if(end){
+            btn1.disabled = true;
+            btn2.disabled = true;
+            btn3.disabled = true;
+        }
+        // if(pcScore === 5){
+        //     final.textContent = "YOU WIN!";
+        //     btn.disabled = true;
+        // }          
+        // else if(npcScore === 5){
+        //     final.textContent = "COMPUTER WINS!";
+        //     btn.disabled = true;
+        // }
+    })
+})
+
+function endScore(){
+    if(pcScore === 5){
+        final.textContent = "YOU WIN!";
+        return true;
+    }          
+    else if(npcScore === 5){
+        final.textContent = "COMPUTER WINS!";  
+        return true;
     }
 }
 
-game();
